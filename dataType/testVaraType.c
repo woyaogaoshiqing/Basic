cab471ee51e9d8075a5768bbe0b58d71b3560982
woyaogaoshiqing <402824251@qq.com>
2023-12-08 14:57:10 +0800

    [新增]1.数据类型 2.变量的作用域和生命周期

---
+++ dataType/testVaraType.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main()
+    {   
+        /*int a=1;
+        int len=sizeof(a);
+        printf("len : %d a = %d \n", len,a);
+        
+    
+        float weigh=50.5;
+        len=sizeof(weigh);
+        printf("len : %d weigh = %f \n", len, weigh);
+
+        double weight=50.5;
+        len = sizeof(weight);
+        printf("len : %d weight = %f \n", len, weight);
+
+        char sex=-1;
+        len =sizeof(sex);
+        printf("len : %d sex = %c sex =%d\n", len , sex ,sex);
+
+        char default_value = 137;
+        len =sizeof(default_value);
+        printf("len : %d default_value = %c default_value =%d\n", len , default_value ,default_value);
+
+        unsigned char m_value =-1;
+        len =sizeof( m_value);
+        printf("len : %d  m_value = %c  m_value =%d\n", len ,  m_value , m_value);*/
+
+       /* int val = 10;
+        printf ("val: %d \n ", val );
+        val++;
+        printf("val: %d \n " , val );
+        val--;
+        printf("val: %d \n " , val );
+
+        ++val;
+        printf("val: %d \n" , val );
+        --val;
+        printf("val: %d \n" , val );*/
+
+        /*int m_value = 10;
+        int discuss = m_value/3;
+        int remaider = m_value%3;
+         printf("discuss: %d \n" , discuss );
+         printf("remaider: %d \n" , remaider );
+
+
+
+        int randomVal =rand();
+        printf("randomVal: %d \n" , randomVal );
+
+        int lotteryRate = randomVal % 4;
+        printf("lotteryRate: %d \n" , lotteryRate );*/
+
+        /*int  val =0;
+       
+        int  orVal =val | 7;
+        int andVal =val & 2;
+        int notVal = !val; 
+        printf("orVal: %d \n" , orVal );
+        printf("andVal: %d \n" , andVal );
+        printf("notVal: %d \n" , notVal );*/
+
+
+        /*int condition1 = 1;
+        int condition2 = 6;
+        
+        if( (condition1 == 1) && (condition2 == 5) )
+        {
+            printf("hello world\n");
+        }
+        else
+        {
+            printf("not hello world\n"); 
+        }*/
+
+        /*int a=10;                                 
+        printf("%p\n",&a);
+        printf("%d\n",a);
+        int *p = & a ;
+        *p=20;
+        printf("%d\n",a);*/
+           
+           
+           
+       /*printf("%zd\n",sizeof(char*));      //指针变量有多少位//
+        printf("%zd\n",sizeof(int*));
+        printf("%zd\n",sizeof(float*));
+        printf("%zd\n",sizeof(double*));
+        printf("%zd\n",sizeof(short*));*/
+
+
+
+      /*双目运算符*/                                                                  
+        int num1 = 10;
+        int num2 =20;
+         /*三目运算符*/
+        int num3 = num1 > num2 ? 100 :666;
+        printf("num3:%d\n",num3);
+
+
+
+
+
+
+
+
+
+
+
+
+        return 0;
+}
